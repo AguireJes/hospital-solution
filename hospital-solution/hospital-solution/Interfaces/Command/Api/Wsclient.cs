baefbce0cf54afacc52d2018d625603bc1ff0ad2
Jesus Edgardo Aguirre <jesuspma06@gmail.com>
2020-04-15 16:09:13 -0500

    Creacion de interfaces para el manejo de base de datos

---
+++ hospital-solution/hospital-solution/Interfaces/Command/Api/Wsclient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital_solution.Interfaces.Command.Api
+{
+    interface Wsclient
+    {
+        Boolean SignIn(String user, String pass);
+    }
+}
---
+++ hospital-solution/hospital-solution/Interfaces/Command/Service/WsclientImpl.cs
@@ -0,0 +1,46 @@
+﻿using hospital_solution.Class.Connection.Service;
+using hospital_solution.Interfaces.Command.Api;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace hospital_solution.Interfaces.Command.Service
+{
+    public class WsclientImpl : Wsclient
+    {
+        public bool SignIn(string user, string pass)
+        {
+            Boolean response = false;
+            try
+            {
+                ConnectionImpl connection = new ConnectionImpl();
+                MySqlConnection connectionBd = connection.getConnection();
+
+                string query = "select count(1) from hospital.login where username = @username and password = @password";
+                
+                MySqlCommand cmd;
+                cmd = new MySqlCommand(query, connectionBd);
+                cmd.Parameters.AddWithValue("@username", user);
+                cmd.Parameters.AddWithValue("@password", pass);
+                cmd.ExecuteNonQuery();
+                
+                int countUser = Convert.ToInt32(cmd.ExecuteScalar());
+                connectionBd.Close();
+                
+                if (countUser > 0)
+                {
+                    response = true;
+                }
+            }
+            catch (Exception exc)
+            {
+                showMessage("Error Message", exc.Message);
+            }
+            return response;
+    }
+
+        private static void showMessage(string v, string message)
+        {
+            throw new NotImplementedException(message);
+        }
+    }
+}
