9d3260c37fd16a4f201fd0a0793f41b333616525
Jesus Edgardo Aguirre <jesuspma06@gmail.com>
2020-04-17 23:23:55 -0500

    Ajustes en el DTO

--- hospital-solution/hospital-solution/Model/PatientDTO.cs
+++ hospital-solution/hospital-solution/Model/PatientDTO.cs
@@ -9,19 +9,20 @@ namespace hospital_solution.Model
     {
         public string name { get; set; }
         public string image { get; set; }
+        public string sex { get; set; }
         public string documentType { get; set; }
         public string telephone { get; set; }
         public string bloodType { get; set; }
         public string email { get; set; }
-        public char country { get; set; }
-        public char district { get; set; }
-        public char township { get; set; }
-        public char slum { get; set; }
-        public char housingType { get; set; }
+        public string country { get; set; }
+        public string district { get; set; }
+        public string township { get; set; }
+        public string slum { get; set; }
+        public string housingType { get; set; }
         public string building { get; set; }
-        public char symptomatic { get; set; }
-        public List<char> travelCountry { get; set; }
-        public char personContact { get; set; }
+        public string symptomatic { get; set; }
+        public List<string> travelCountry { get; set; }
+        public string personContact { get; set; }
         public string personQuantity { get; set; }
         public string idHouse { get; set; }
     }
