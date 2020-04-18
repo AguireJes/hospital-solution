fa62fc71c9bac1c75a526bc3fc881e4ed3c244bc
Jesus Edgardo Aguirre <jesuspma06@gmail.com>
2020-04-18 10:41:19 -0500

    Pagina nueva

--- hospital-solution/hospital-solution/RelationPatient.aspx.cs
+++ hospital-solution/hospital-solution/RelationPatient.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hospital_solution.Interfaces.Command.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,44 @@ namespace hospital_solution
 {
     public partial class RelationPatient : System.Web.UI.Page
     {
+        WsclientImpl wsClientImpl = new WsclientImpl();
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadCatalogs();
+        }
+
+        private void LoadCatalogs()
+        {
+            LoadSexCatalog();
+            LoadBloodCatalog();
+            LoadCountryCatalog();
+        }
+
+        private void LoadSexCatalog()
+        {
+            string query = "select * from hospital.sexo";
+            wsClientImpl.LoadDropDownList(dropDownList: sexType, query, "valor", "descripcion");
+        }
+
+        private void LoadBloodCatalog()
+        {
+            string query = "select * from hospital.tiposangre";
+            wsClientImpl.LoadDropDownList(dropDownList: bloodType, query, "idtiposangre", "sangre");
+        }
+
+        private void LoadCountryCatalog()
+        {
+            string query = "select * from hospital.paises";
+            wsClientImpl.LoadDropDownList(dropDownList: countryChoice, query, "idpaises", "pais");
+        }
+
+        protected void saveContact_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fowardForm_Click(object sender, EventArgs e)
         {
 
         }
